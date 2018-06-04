2bb981eb65b00f79b69bb715212ab748f64603b5
Anna Sobolewska <anna@whistle.com>
2018-06-03 18:20:48 -0700

    Monetize summary payments and charges

--- app/models/summary.rb
+++ app/models/summary.rb
@@ -4,4 +4,8 @@ class Summary < ApplicationRecord
   validates :average_covered_charges, presence: true
   validates :average_total_payments, presence: true
   validates :average_medicare_payments, presence: true
+
+  monetize :average_covered_charges, as: 'average_covered_charges_amount'
+  monetize :average_total_payments, as: 'average_total_payments_amount'
+  monetize :average_medicare_payments, as: 'average_medicare_payments_amount'
 end
--- spec/models/summary_spec.rb
+++ spec/models/summary_spec.rb
@@ -8,4 +8,10 @@ RSpec.describe Summary, type: :model do
     it { is_expected.to validate_presence_of(:average_total_payments) }
     it { is_expected.to validate_presence_of(:average_medicare_payments) }
   end
+
+  context 'monetize' do
+    it { is_expected.to monetize(:average_covered_charges).as(:average_covered_charges_amount) }
+    it { is_expected.to monetize(:average_total_payments).as(:average_total_payments_amount) }
+    it { is_expected.to monetize(:average_medicare_payments).as(:average_medicare_payments_amount) }
+  end
 end
