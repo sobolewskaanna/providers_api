e1cd156a11a8ff2ae9f50d3b0e11aff3c4935654
Anna Sobolewska <anna@whistle.com>
2018-06-03 17:49:00 -0700

    Add Provider factory

---
+++ spec/factories.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :provider do
+    provider_id 1001
+    name 'SOUTHEAST ALABAMA MEDICAL CENTER'
+    street_address '1108 ROSS CLARK CIRCLE'
+    city 'San Francisco'
+    state 'CA'
+    zip_code 94_103
+    hospital_referral_region_description 'CA - San Francisco'
+  end
+end
